6a71c4d38d954793c58b413cdd0bf63b12cc1a36
hephaistionn <alexandre.monge.mail@gmail.com>
2018-12-09 15:46:43 +0100

    fix : MaterialMap Shader fix : normal direction

--- client/view/app/material/materialA.js
+++ client/view/app/material/materialA.js
@@ -2,7 +2,8 @@ const THREE = require('three');
 const material = new THREE.MeshPhongMaterial({
     map: THREE.ImageUtils.loadTexture('pic/entity.png'),
     //side: THREE.DoubleSide,
-    color: 0x666666,
+    //color: 0x666666,
+    //flatShading: true,
     shininess: 0,
 });
 module.exports = material;
--- client/view/app/material/materialMap.js
+++ client/view/app/material/materialMap.js
@@ -1,15 +1,12 @@
 const THREE = require('three');
 
 const vertShader = "" +
-    "varying vec3 vecNormal; \n" +
+    "varying vec3 vNormal; \n" +
     "varying vec3 vAbsolutePosition; \n" +
-    "uniform mat4 directionalShadowMatrix[ NUM_DIR_LIGHTS ]; \n" +
-    "varying vec4 vDirectionalShadowCoord; \n" +
     "void main() { \n" +
     "vec4 worldPosition = modelMatrix * vec4(position, 1.0 ); \n" +
     "vAbsolutePosition = worldPosition.xyz; \n" +
-    "vecNormal = (modelMatrix * vec4(normal, 0.0)).xyz; \n" +
-    "vDirectionalShadowCoord = directionalShadowMatrix[ 0 ] * worldPosition; \n" +
+    "vNormal = normalMatrix * normal; \n" +
     "gl_Position = projectionMatrix * viewMatrix * worldPosition; \n" +
     "} ";
 
@@ -25,28 +22,9 @@ const fragShader = "" +
     "   vec2 shadowMapSize; \n" +
     "}; \n" +
     "uniform DirectionalLight directionalLights[ NUM_DIR_LIGHTS ]; \n" +
-    "uniform sampler2D directionalShadowMap[ NUM_DIR_LIGHTS ]; \n" +
     "uniform float textureSize; \n" +
-    "varying vec4 vDirectionalShadowCoord; \n" +
-    "float unpackDepth( const in vec4 rgba_depth ) { \n" +
-    "    const vec4 bit_shift = vec4( 1.0 / ( 256.0 * 256.0 * 256.0 ), 1.0 / ( 256.0 * 256.0 ), 1.0 / 256.0, 1.0 ); \n" +
-    "    return dot( rgba_depth, bit_shift ); \n" +
-    "} \n" +
-    "float texture2DCompare( sampler2D depths, vec2 uv, float compare ) { \n" +
-    "    return step( compare, unpackDepth( texture2D( depths, uv ) ) ); \n" +
-    "} \n" +
-    "float getShadow( sampler2D shadowMap, vec2 shadowMapSize, float shadowBias, float shadowRadius, vec4 shadowCoord ) { \n" +
-    "    shadowCoord.xyz /= shadowCoord.w; \n" +
-    "    shadowCoord.z += shadowBias; \n" +
-    "    bvec4 inFrustumVec = bvec4 ( shadowCoord.x >= 0.0, shadowCoord.x <= 1.0, shadowCoord.y >= 0.0, shadowCoord.y <= 1.0 ); \n" +
-    "    bool inFrustum = all( inFrustumVec ); \n" +
-    "    if ( inFrustum ) { \n" +
-    "        return max(texture2DCompare( shadowMap, shadowCoord.xy, shadowCoord.z ),0.5); \n" +
-    "    } \n" +
-    "    return 1.0; \n" +
-    "} \n" +
 
-    "varying vec3 vecNormal; \n" +
+    "varying vec3 vNormal; \n" +
     "varying vec3 vAbsolutePosition; \n" +
     "uniform sampler2D texture; \n" +
     "" +
@@ -57,11 +35,10 @@ const fragShader = "" +
     "   vec3 colorFinal = texture2D( texture, UV ).xyz;"+
     "   vec3 sumLights = vec3(0.0, 0.0, 0.0); \n" +
     "   DirectionalLight directionalLight;" +
+    "   vec3 normal = normalize( vNormal );"+
     "   for(int i = 0; i < NUM_DIR_LIGHTS; i++) {\n" +
     "       directionalLight = directionalLights[ i ]; \n" +
-    "       sumLights += dot(directionalLight.direction, vecNormal)* directionalLight.color; \n" +
-    "           float shadowFactor = bool( directionalLight.shadow ) ? getShadow( directionalShadowMap[ i ], directionalLight.shadowMapSize, directionalLight.shadowBias, directionalLight.shadowRadius, vDirectionalShadowCoord ) : 1.0; \n" +
-    "           sumLights *= shadowFactor; \n" +
+    "       sumLights += dot(directionalLight.direction, normal)* directionalLight.color; \n" +
     "   } \n" +
     "   sumLights = ambientLightColor + max(vec3(0.0,0.0,0.0),sumLights); \n" +
     "   colorFinal *= sumLights; \n" +
