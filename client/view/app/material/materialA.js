d5ede483fc5760285db6f0038d24357451856dfd
hephaistionn <alexandre.monge.mail@gmail.com>
2018-12-14 13:53:03 +0100

    fix : warning threejs texture loader

--- client/view/app/material/materialA.js
+++ client/view/app/material/materialA.js
@@ -1,6 +1,6 @@
 const THREE = require('three');
 const material = new THREE.MeshPhongMaterial({
-    map: THREE.ImageUtils.loadTexture('pic/entity.png'),
+    map:  new THREE.TextureLoader().load('pic/entity.png'),
     //side: THREE.DoubleSide,
     //color: 0x666666,
     //flatShading: true,
