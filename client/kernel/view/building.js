07f044e48c0bec61c94f2d3379a64c3f3c7d04c4
hephaistionn <alexandre.monge.mail@gmail.com>
2019-01-30 23:26:57 +0100

    materials in foundation

--- client/kernel/view/building.js
+++ client/kernel/view/building.js
@@ -2,8 +2,31 @@ const THREE = require('three');
 
 const colorBlue = 0x0000ff;
 const colorRed = 0xff0000;
+const materialA = require('../../view/app/material/materialA');
 const meshSelector = new THREE.BoxHelper(undefined, 0xffff00);
 meshSelector.matrixAutoUpdate = false;
+const Stats = require('../model/stats');
+const obj = {};
+obj[Stats.WOOD] =  [
+	'obj/resources/wood_00.obj',
+	'obj/resources/wood_01.obj',
+	'obj/resources/wood_02.obj',
+	'obj/resources/wood_03.obj'
+];
+
+obj[Stats.STONE] =  [
+	'obj/resources/stone_00.obj',
+	'obj/resources/stone_01.obj',
+	'obj/resources/stone_02.obj',
+	'obj/resources/stone_03.obj'
+];
+
+const blockPos = [
+	3,3, 1,3, -1,3, -3,3,
+	3,1, 1,1, -1,1, -3,1,
+	3,-1, 1,-1, -1,-1, -3,-1,
+	1,-3, -1,-3, -3,-3
+];
 
 class Building {
 
@@ -12,6 +35,8 @@ class Building {
         this.element.matrixAutoUpdate = false;
         this.boxSelector = null;
         this.meshLevel = [];
+        this.indexblock = 0;
+        this.materials = [];
         this.level = 99;
         this.add(parent);
         this.initMesh(model);
@@ -45,8 +70,56 @@ class Building {
             if (model.selected || this.boxSelector) {
                 this.updateMeshSelector(model);
             }
+            if(model.level ===  0)  {
+                this.removeMaterial();
+                this.indexblock = 0;
+                if(model.materials[Stats.WOOD])
+                    this.updateMaterilas(Stats.WOOD, model);
+        
+                if(model.materials[Stats.STONE])
+                    this.updateMaterilas(Stats.STONE, model);
+            }
         }
     }
+
+    removeMaterial() {
+		for(let i=0; i<this.materials.length;  i++) {
+			if(this.materials[i]) {
+				this.element.remove(this.materials[i]);
+				this.materials[i] = null;
+			}
+		}
+    }
+
+	updateMaterilas(type, model) {
+		let matrixWorld;
+		let fullBlock = Math.floor(model.materials[type]/15);
+		const leadBlock = Math.floor(model.materials[type]%15);
+		while(fullBlock--) {
+			this.materials[this.indexblock] = THREE.getMesh(obj[type][3], materialA);
+			matrixWorld = this.materials[this.indexblock].matrixWorld.elements;
+			matrixWorld[12] = blockPos[this.indexblock*2] + model.ax;
+			matrixWorld[14] = blockPos[this.indexblock*2+1] + model.az;
+			matrixWorld[13] = 0.5 + model.ay;
+			this.meshLevel[this.level].add(this.materials[this.indexblock]);
+			this.indexblock++;
+		}
+
+		if(leadBlock > 8) {
+			this.materials[this.indexblock] = THREE.getMesh(obj[type][2], materialA);
+		} else if (leadBlock > 4) {
+			this.materials[this.indexblock] = THREE.getMesh(obj[type][1], materialA);
+		} else if (leadBlock > 0) {
+			this.materials[this.indexblock] = THREE.getMesh(obj[type][0], materialA);
+		}
+
+		matrixWorld = this.materials[this.indexblock].matrixWorld.elements;
+		matrixWorld[12] = blockPos[this.indexblock*2] + model.ax;
+		matrixWorld[14] = blockPos[this.indexblock*2+1] + model.az;
+		matrixWorld[13] = 0.5 + model.ay;
+		this.meshLevel[this.level].add(this.materials[this.indexblock]);
+		this.indexblock++;
+	}
 
     updateMeshSelector(model) {
         if (model.selected && !this.boxSelector) {
