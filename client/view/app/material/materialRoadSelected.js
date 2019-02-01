a4bda5cae41a91fcdf97445205cb5d6792224495
hephaistionn <alexandre.monge.mail@gmail.com>
2019-02-01 17:11:02 +0100

    road select shader

--- client/view/app/material/materialRoadSelected.js
+++ client/view/app/material/materialRoadSelected.js
@@ -1,33 +1,41 @@
 const THREE = require('three');
 
 const vertShader = "" +
+    "precision highp float; \n" +
+    "precision highp int; \n" +
+    "uniform mat4 modelMatrix; \n" +
+    "uniform mat4 viewMatrix;\n" +
+    "uniform mat4 projectionMatrix;\n" +
     "attribute float walkable; \n" +
+    "attribute vec3 position; \n" +
     "varying float vColor; \n" +
-    "void main() { \n" +
-    "vec4 worldPosition = modelMatrix * vec4(position, 1.0 ); \n" +
-    "gl_Position = projectionMatrix * viewMatrix * worldPosition; \n" +
-    "vColor = walkable; \n" +
+    "void main() {" +
+    "   vec4 worldPosition = modelMatrix * vec4(position, 1.0 ); \n"+
+    "   vColor = walkable; \n" +
+    "   gl_Position = projectionMatrix * viewMatrix * worldPosition; \n"+
     "} ";
 
 
 const fragShader = "" +
+    "precision highp float; \n" +
+    "precision highp int; \n" +
+    "uniform sampler2D map; \n"+
     "varying float vColor; \n" +
-    "" +
     "void main(void) { \n" +
-    "vec3 colorFinal = vec3(0.0,0.0,1.0);" +
-    "if(vColor<1.0){ \n" +
-    "   colorFinal = vec3(1.0,0.0,0.0); \n" +
-    "} \n" +
-    "" +
-    "gl_FragColor = vec4(colorFinal,0.5); \n" +
-    "} ";
+    "   if(vColor<1.0){ \n" +
+    "       gl_FragColor =  vec4(1.0, 0.0, 0.0, 0.5); \n" +
+    "   } else { \n" +
+    "       gl_FragColor =  vec4(0.0, 0.0, 1.0, 0.5); \n" +
+    "   }  \n"+
+    "}";
 
-const mat = new THREE.ShaderMaterial({
-    uniforms: {},
+const uniforms = THREE.UniformsUtils.merge([]);
+const material = new THREE.RawShaderMaterial({
+    uniforms: uniforms,
     vertexShader: vertShader,
     fragmentShader: fragShader,
-    lights: false,
     transparent: true
 });
 
-module.exports = mat;
+module.exports = material;
+
