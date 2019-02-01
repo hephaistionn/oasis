32431142fee619d7bef5cdb23222cb0b96caeb34
hephaistionn <alexandre.monge.mail@gmail.com>
2019-02-01 16:36:28 +0100

    custom main shader

--- client/view/app/material/materialA.js
+++ client/view/app/material/materialA.js
@@ -1,10 +1,47 @@
 const THREE = require('three');
-const material = new THREE.MeshPhongMaterial({
-    map:  new THREE.TextureLoader().load('pic/texture_00.jpg'),
-    side: THREE.DoubleSide,
-    //color: 0x666666,
-    //flatShading: true,
-    //emissive: 0x444444,
-    shininess: 0,
+
+const vertShader = "" +
+    "precision highp float; \n" +
+    "precision highp int; \n" +
+    "uniform mat4 modelMatrix; \n" +
+    "uniform mat4 viewMatrix;\n" +
+    "uniform mat4 projectionMatrix;\n" +
+    "attribute vec3 position; \n" +
+    "attribute vec3 normal; \n" +
+    "attribute vec2 uv; \n" +
+    "varying vec3 vNormal; \n" +
+    "varying vec2 vUv; \n" +
+    "void main() {" +
+    "   vec4 worldPosition = modelMatrix * vec4(position, 1.0 ); \n" +
+    "   vNormal = (modelMatrix * vec4(normal, 0.0)).xyz; \n" +
+    "   vUv = uv;  \n" +
+    "   gl_Position = projectionMatrix * viewMatrix * worldPosition; \n" +
+    "} ";
+
+
+const fragShader = "" +
+    "precision highp float; \n" +
+    "precision highp int; \n" +
+    "varying vec3 vNormal; \n" +
+    "varying vec2 vUv; \n" +
+    "uniform sampler2D map; \n"+
+    "" +
+    "void main(void) { \n" +
+    "   vec3 ambientLightColor = vec3(0.6);\n" +
+    "   vec3 lightDirection = vec3(0.2585,0.9307,-0.2585);\n" +
+    "   vec4 texelColor = texture2D( map, vUv );\n" +
+    "   float light = dot(lightDirection, normalize( vNormal ));\n"+
+    "   vec3 color = max(texelColor.xyz  * light, texelColor.xyz * ambientLightColor); \n" +
+    "   gl_FragColor = vec4(color, texelColor.a); \n" +
+    "}";
+
+const uniforms = THREE.UniformsUtils.merge([]);
+uniforms.map = {type: 't', value: new THREE.TextureLoader().load('pic/texture_00.jpg')};
+const material = new THREE.RawShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: vertShader,
+    fragmentShader: fragShader
 });
+
 module.exports = material;
+
